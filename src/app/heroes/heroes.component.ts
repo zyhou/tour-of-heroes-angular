d6f16a78a66a7312e1c132e1af46c9e08a1e6eae
Maxime <richard.maxime9@gmail.com>
2016-11-27 19:45:00 +0100

    Modification service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,8 +5,7 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  providers: [HeroService]
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
